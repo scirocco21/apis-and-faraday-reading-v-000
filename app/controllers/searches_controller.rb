e1edd403f551808048a132a6c4891589a89eb66a
Seb Gertz <sebgertz@gmail.com>
2018-08-23 18:32:31 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,7 +10,6 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
       req.options.timeout = 0
-      end
 
     body = JSON.parse(@resp.body)
     if @resp.success?
